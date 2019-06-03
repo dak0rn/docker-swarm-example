adc9f11a65a1b316e63153a03001840bde025d7f
Daniel Koch <dk@g9h.co>
2019-06-03 19:22:48 +0200

    Implement server sending for the collector

--- collector/main.go
+++ collector/main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	docker "github.com/docker/docker/client"
 	log "github.com/sirupsen/logrus"
+	"net/http"
+	"os"
 	"time"
 )
+
+type DockerSwarmInfo struct {
+	NodeID         string
+	NodeAddr       string
+	LocalNodeState string
+	Error          string
+}
+
+type DockerHostInfo struct {
+	ID                string
+	Containers        int
+	ContainersRunning int
+	ContainersPaused  int
+	ContainersStopped int
+	Images            int
+	SystemTime        string
+	Name              string
+	Swarm             DockerSwarmInfo
+}
+
+// Reads the given flag from the environment
+// Panics, if it is unset
+func readenv(name string) string {
+	value, has := os.LookupEnv(name)
+	if !has {
+		panic(fmt.Sprintf("Environment variable %s not set", name))
+	}
+
+	return value
+}
 
 // Connects to the Docker host using parameter set in the environment
 // and returns the connected client
@@ -27,7 +61,7 @@ func handleErr(err error, msg string) {
 
 // Collects host information and sends it to the given
 // master node
-func collectAndSend(client *docker.Client) {
+func collectAndSend(client *docker.Client, storeAddr string) {
 	log.Info("Collecting host information...")
 
 	info, err := client.Info(context.Background())
@@ -36,15 +70,36 @@ func collectAndSend(client *docker.Client) {
 		return
 	}
 
-	jsonbytes, err := json.Marshal(info)
+	var serverData = DockerHostInfo{}
+	serverData.Swarm = DockerSwarmInfo{}
+
+	serverData.ID = info.ID
+	serverData.Containers = info.Containers
+	serverData.ContainersRunning = info.ContainersRunning
+	serverData.ContainersPaused = info.ContainersPaused
+	serverData.ContainersStopped = info.ContainersStopped
+	serverData.Images = info.Images
+	serverData.SystemTime = info.SystemTime
+	serverData.Name = info.Name
+	serverData.Swarm.NodeID = info.Swarm.NodeID
+	serverData.Swarm.NodeAddr = info.Swarm.NodeAddr
+	serverData.Swarm.LocalNodeState = string(info.Swarm.LocalNodeState)
+	serverData.Swarm.Error = info.Swarm.Error
+
+	jsonbytes, err := json.Marshal(serverData)
 	if nil != err {
 		handleErr(err, "Failed to encode the server info as JSON")
 		return
 	}
 
-	log.Info("Data", string(jsonbytes))
+	log.Info("Sending data to the store...")
+	_, err = http.Post(storeAddr, "application/json", bytes.NewReader(jsonbytes))
+	if nil != err {
+		handleErr(err, "Failed to send data to the store")
+		return
+	}
 
-	// TODO Send information to the master node using HTTP(S)
+	log.Info("Information has been send.")
 }
 
 func main() {
@@ -57,11 +112,11 @@ func main() {
 	log.Info("Connecting to Docker...")
 	client := dockerConnect()
 
-	// TODO Fetch the master node address from the environment
+	storeAddr := readenv("VIEWER_ADDR")
 
 	log.Info("Starting the collection loop...")
 	for {
-		collectAndSend(client)
+		collectAndSend(client, storeAddr)
 		<-time.After(10 * time.Second)
 	}
 }
